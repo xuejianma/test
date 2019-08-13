2a3b64070729c0ab8dfbebcba05a7b8f91490871
xuejianma <xuejian.ma@gmail.com>
2019-08-12 22:20:58 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 print(123)
 import matplotlib as mpl
 mpl.use('Agg')
-from matplotlib import pylot 
+import matplotlib.pyplot import as plt 
 plt.ioff()
 ax = plt.figure()
 plt.plot([1,2,3],[4,5,6])
